64bf25930b398bbe70f0c7eb8606ecf26496fc8f
Endy Jasmi <endyjasmi@gmail.com>
2015-08-15 09:26:25 +0800

    Updated with directory structure.

---
+++ application/application.js
@@ -0,0 +1,12 @@
+var application = angular.module('application', [
+    'ngAnimate',
+    'ngAria',
+    'ngMaterial',
+    'ngMessageFormat',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'pascalprecht.translate', // angular-translate
+    'application.templates', // application templates cache
+]);
--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,19 @@ var path = require('path');
 
 // Plugin imports
 var concat = require('gulp-concat');
+var connect = require('gulp-connect');
+var htmlmin = require('gulp-htmlmin');
 var minifyCss = require('gulp-minify-css');
 var sourcemaps = require('gulp-sourcemaps');
+var templateCache = require('gulp-angular-templatecache');
 var uglify = require('gulp-uglify');
 
 // Settings
 var settings = {
     file: {
-        stylesheets: 'app.min.css',
+        javascripts: 'application.min.js',
+        stylesheets: 'application.min.css',
+        templates: 'templates.min.js',
         vendor: {
             javascripts: 'vendor.min.js',
             stylesheets: 'vendor.min.css',
@@ -24,8 +29,24 @@ var settings = {
             fonts: path.join(process.cwd(), 'vendor', 'fonts'),
         },
     },
+    htmls: [
+        'index.html',
+    ],
+    javascripts: [
+        'application/application.js',
+        'application/translations/*.js',
+        'application/controllers/*.js',
+        'application/directives/*.js',
+        'application/filters/*.js',
+        'application/providers/*.js',
+        'application/services/*.js',
+    ],
     stylesheets: [
         'stylesheets/material-design-icons.css',
+        'stylesheets/application.css',
+    ],
+    templates: [
+        'templates/*.html',
     ],
     vendor: {
         fonts: [
@@ -52,9 +73,41 @@ var settings = {
 };
 
 // Tasks
-gulp.task('build', ['stylesheets']);
+gulp.task('build', [
+    'htmls',
+    'javascripts',
+    'stylesheets',
+    'templates',
+]);
 
-gulp.task('default', ['build', 'vendor']);
+gulp.task('default', [
+    'build',
+    'vendor'
+]);
+
+gulp.task('htmls', function()
+{
+    return gulp.src(settings.htmls)
+        .pipe(connect.reload());
+});
+
+gulp.task('javascripts', function()
+{
+    return gulp.src(settings.javascripts)
+        .pipe(sourcemaps.init())
+        .pipe(concat(settings.file.javascripts))
+        .pipe(uglify())
+        .pipe(sourcemaps.write(settings.folder.root))
+        .pipe(gulp.dest(settings.folder.root))
+        .pipe(connect.reload());
+});
+
+gulp.task('server', function()
+{
+    return connect.server({
+        livereload: true,
+    });
+});
 
 gulp.task('stylesheets', function()
 {
@@ -63,10 +116,29 @@ gulp.task('stylesheets', function()
         .pipe(concat(settings.file.stylesheets))
         .pipe(minifyCss())
         .pipe(sourcemaps.write(settings.folder.root))
-        .pipe(gulp.dest(settings.folder.root));
+        .pipe(gulp.dest(settings.folder.root))
+        .pipe(connect.reload());
 });
 
-gulp.task('vendor', ['vendor:fonts', 'vendor:javascripts', 'vendor:stylesheets']);
+gulp.task('templates', function()
+{
+    var options = {
+        module: 'application.templates',
+        standalone: true,
+    };
+
+    return gulp.src(settings.templates)
+        .pipe(htmlmin())
+        .pipe(templateCache(settings.file.templates, options))
+        .pipe(gulp.dest(settings.folder.root))
+        .pipe(connect.reload());
+});
+
+gulp.task('vendor', [
+    'vendor:fonts',
+    'vendor:javascripts',
+    'vendor:stylesheets',
+]);
 
 gulp.task('vendor:fonts', function()
 {
@@ -92,4 +164,40 @@ gulp.task('vendor:stylesheets', function()
         .pipe(minifyCss())
         .pipe(sourcemaps.write(settings.folder.vendor.root))
         .pipe(gulp.dest(settings.folder.vendor.root));
+});
+
+gulp.task('watch', [
+    'server',
+    'watch:htmls',
+    'watch:javascripts',
+    'watch:stylesheets',
+    'watch:templates',
+]);
+
+gulp.task('watch:htmls', function()
+{
+    return gulp.watch(settings.htmls, [
+        'htmls',
+    ]);
+});
+
+gulp.task('watch:javascripts', function()
+{
+    return gulp.watch(settings.javascripts, [
+        'javascripts',
+    ]);
+});
+
+gulp.task('watch:stylesheets', function()
+{
+    return gulp.watch(settings.stylesheets, [
+        'stylesheets',
+    ]);
+});
+
+gulp.task('watch:templates', function()
+{
+    return gulp.watch(settings.templates, [
+        'templates',
+    ]);
 });
